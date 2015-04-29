543247334529c041c0e6c46a2f3f9f7b40dc3e94
Bruno Alves <bruno.ar.avl@gmail.com>
2015-04-28 23:53:25 -0300

    #Codificação do formulário de confirmaNotaFiscal 35%

--- Sistema Matrix/Confirma Nota Fiscal.vb
+++ Sistema Matrix/Confirma Nota Fiscal.vb
@@ -25,10 +25,12 @@ Public Class confirmaNotaFiscal
         Next
     End Sub
     Private Sub Nota_Fiscal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'codOrdemAtual = 8
+        codOrdemAtual = 8
 
+        'Armazena o código do cliente
         Dim codCliente As Integer
 
+        'Verifica o código do cliente que abriu a ordem
         strSQL = "SELECT cliCodigo FROM ordemServico WHERE ordCodigo = " & codOrdemAtual
         leitor = objBanco.ExecutaDataRead(strSQL)
         leitor.Read()
@@ -36,18 +38,18 @@ Public Class confirmaNotaFiscal
 
         'Executa a tribuição das tags
         atribuiTag(Me)
+        'Retira a tag da textbox que não necessita estar desabilitada
         txtOutrasInformacoes.Tag = ""
         'Desabilita os campos de preenchimento automático
         habilitaDesabilitaControles(Me, True)
 
         Try
-
-
             'Carrega os dados do presatador
             strSQL = "SELECT demCodigo, demRazaoSocial, demInscricaoMunicipal, demEndereco FROM dadosEmitente"
             leitor = objBanco.ExecutaDataRead(strSQL)
             leitor.Read()
 
+            'Atribui os valores retornados na consulta as text box
             txtCnpjCpfTomador.Text = leitor.Item(0).ToString
             txtRazaoSocialNomeTomador.Text = leitor.Item(1).ToString
             txtInscricaoMunicipalTomador.Text = leitor.Item(2).ToString
@@ -56,10 +58,11 @@ Public Class confirmaNotaFiscal
             txtUFTomador.Text = "SP"
 
             'Carrega os dados do tomador
-            strSQL = "SELECT cliTipo FROM cliente"
+            strSQL = "SELECT cliTipo FROM cliente WHERE cliCodigo = " & codCliente
             leitor = objBanco.ExecutaDataRead(strSQL)
             leitor.Read()
 
+            'Verifica se o cliente é pessoa física ou jurídica
             If leitor.Item(0).ToString = "F" Then
                 strSQL = "SELECT cliPessoaFisica.cliCPF, cliente.cliNome, cliente.cliEndereco, cliente.cliCidade, cliente.cliUF, cliente.cliEmail FROM cliente INNER JOIN cliPessoaFisica ON cliente.cliCodigo = cliPessoaFisica.cliCodigo WHERE cliente.cliCodigo = " & codCliente
                 leitor = objBanco.ExecutaDataRead(strSQL)
@@ -72,7 +75,32 @@ Public Class confirmaNotaFiscal
                 txtUFPrestador.Text = leitor.Item(4).ToString
                 txtEmailPrestador.Text = leitor.Item(5).ToString
                 txtInscricaoMunicipalPrestador.Text = "-"
+            ElseIf leitor.Item(0).ToString = "J" Then
+                strSQL = "SELECT cliPessoaJuridica.cliCNPJ, cliPessoaJuridica.cliRazaoSocial, cliPessoaJuridica.cliIM, cliente.cliEndereco, cliente.cliCidade, cliente.cliUF, cliente.cliEmail FROM cliente INNER JOIN cliPessoaJuridica ON cliente.cliCodigo = cliPessoaJuridica.cliCodigo WHERE cliente.cliCodigo = " & codCliente
+                leitor = objBanco.ExecutaDataRead(strSQL)
+                leitor.Read()
+                txtCnpjCpfPrestador.Text = leitor.Item(0).ToString
+                txtRazaoSocialNomePrestador.Text = leitor.Item(1).ToString
+                txtInscricaoMunicipalPrestador.Text = leitor.Item(2).ToString
+                txtEnderecoPrestador.Text = leitor.Item(3).ToString
+                txtMunicipioPrestador.Text = leitor.Item(4).ToString
+                txtUFPrestador.Text = leitor.Item(5).ToString
+                txtEmailPrestador.Text = leitor.Item(6).ToString
+            Else
+                MsgBox("Ocorreu um erro na gravação do registro do cliente atual, por favor, reveja o cadastro deste cliente", vbExclamation, "Erro")
+                Me.Close()
             End If
+
+            'Carrega a lista com serviços
+            strSQL = "SELECT servicos_os.seoQuantidade, servicos.svcNome, servicos.svcCodigoServico, servicos.svcNome, servicos.svcAliquota, servicos.svcValorHora FROM ordemServico INNER JOIN (servicos INNER JOIN servicos_os ON servicos.svcCodigo = servicos_os.svcCodigo) ON ordemServico.ordCodigo = servicos_os.ordCodigo WHERE ordemServico.ordCodigo = " & codOrdemAtual
+            leitor = objBanco.ExecutaDataRead(strSQL)
+            Dim armazenaServico As String
+            'Dim valorTotal As Integer
+            While leitor.Read
+                armazenaServico = leitor.Item(0).ToString & vbTab & leitor.Item(1)
+                'valorTotal = valorTotal + (CInt(leitor.Item(0))
+                lstServicos.Items.Add(armazenaServico)
+            End While
 
         Catch exc As SqlClient.SqlException
             MsgBox("Erro com banco de dados" & vbCrLf & Err.Description, vbCritical, "Erro com Banco de dados")
