7faac57feced71268de9561781f899467d699f8a
Fabiano Luiz <fabianoluiz@hotmail.com>
2015-05-27 11:39:07 -0300

    Melhorado a interface da tela de backup e restauração

--- Sistema Matrix/frmBackupeRestauracao.Designer.vb
+++ Sistema Matrix/frmBackupeRestauracao.Designer.vb
@@ -22,36 +22,37 @@ Partial Class frmBackupeRestauracao
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBackupeRestauracao))
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.btnBackup = New System.Windows.Forms.Button()
-        Me.btnDestino = New System.Windows.Forms.Button()
-        Me.btnBancoDeDados = New System.Windows.Forms.Button()
-        Me.txtNomeBackup = New System.Windows.Forms.TextBox()
-        Me.txtDestino = New System.Windows.Forms.TextBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtBancoDeDados = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.btnRestaurar = New System.Windows.Forms.Button()
-        Me.btnDestinoRestaurar = New System.Windows.Forms.Button()
-        Me.btnArquivoBackupRestaurar = New System.Windows.Forms.Button()
-        Me.txtDestinoRestaurar = New System.Windows.Forms.TextBox()
-        Me.txtArquivoBackupRestaurar = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtDestino = New System.Windows.Forms.TextBox()
+        Me.btnBancodeDados = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnBackup = New System.Windows.Forms.Button()
+        Me.btnRestaurar = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtArquivoBackupRestaurar = New System.Windows.Forms.TextBox()
+        Me.txtDestinoRestaurar = New System.Windows.Forms.TextBox()
+        Me.txtNomeBackup = New System.Windows.Forms.TextBox()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.btnBackup)
-        Me.GroupBox1.Controls.Add(Me.btnDestino)
-        Me.GroupBox1.Controls.Add(Me.btnBancoDeDados)
         Me.GroupBox1.Controls.Add(Me.txtNomeBackup)
+        Me.GroupBox1.Controls.Add(Me.btnBackup)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btnBancodeDados)
         Me.GroupBox1.Controls.Add(Me.txtDestino)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.txtBancoDeDados)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
@@ -63,59 +64,14 @@ Partial Class frmBackupeRestauracao
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Backup"
         '
-        'btnBackup
+        'txtBancoDeDados
         '
-        Me.btnBackup.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnBackup.Location = New System.Drawing.Point(260, 218)
-        Me.btnBackup.Name = "btnBackup"
-        Me.btnBackup.Size = New System.Drawing.Size(245, 66)
-        Me.btnBackup.TabIndex = 8
-        Me.btnBackup.Text = "Iniciar o Backup"
-        Me.btnBackup.UseVisualStyleBackColor = True
-        '
-        'btnDestino
-        '
-        Me.btnDestino.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnDestino.Location = New System.Drawing.Point(623, 120)
-        Me.btnDestino.Name = "btnDestino"
-        Me.btnDestino.Size = New System.Drawing.Size(75, 23)
-        Me.btnDestino.TabIndex = 7
-        Me.btnDestino.Text = "Procurar"
-        Me.btnDestino.UseVisualStyleBackColor = True
-        '
-        'btnBancoDeDados
-        '
-        Me.btnBancoDeDados.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnBancoDeDados.Location = New System.Drawing.Point(623, 54)
-        Me.btnBancoDeDados.Name = "btnBancoDeDados"
-        Me.btnBancoDeDados.Size = New System.Drawing.Size(75, 23)
-        Me.btnBancoDeDados.TabIndex = 6
-        Me.btnBancoDeDados.Text = "Procurar"
-        Me.btnBancoDeDados.UseVisualStyleBackColor = True
-        '
-        'txtNomeBackup
-        '
-        Me.txtNomeBackup.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtNomeBackup.Location = New System.Drawing.Point(133, 163)
-        Me.txtNomeBackup.Name = "txtNomeBackup"
-        Me.txtNomeBackup.Size = New System.Drawing.Size(565, 22)
-        Me.txtNomeBackup.TabIndex = 5
-        '
-        'txtDestino
-        '
-        Me.txtDestino.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtDestino.Location = New System.Drawing.Point(129, 92)
-        Me.txtDestino.Name = "txtDestino"
-        Me.txtDestino.Size = New System.Drawing.Size(569, 22)
-        Me.txtDestino.TabIndex = 4
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(129, 26)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(569, 22)
-        Me.TextBox1.TabIndex = 3
+        Me.txtBancoDeDados.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.txtBancoDeDados.Location = New System.Drawing.Point(129, 21)
+        Me.txtBancoDeDados.Name = "txtBancoDeDados"
+        Me.txtBancoDeDados.ReadOnly = True
+        Me.txtBancoDeDados.Size = New System.Drawing.Size(565, 27)
+        Me.txtBancoDeDados.TabIndex = 9
         '
         'Label3
         '
@@ -149,11 +105,11 @@ Partial Class frmBackupeRestauracao
         '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.btnRestaurar)
-        Me.GroupBox2.Controls.Add(Me.btnDestinoRestaurar)
-        Me.GroupBox2.Controls.Add(Me.btnArquivoBackupRestaurar)
         Me.GroupBox2.Controls.Add(Me.txtDestinoRestaurar)
         Me.GroupBox2.Controls.Add(Me.txtArquivoBackupRestaurar)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.btnRestaurar)
         Me.GroupBox2.Controls.Add(Me.Label5)
         Me.GroupBox2.Controls.Add(Me.Label6)
         Me.GroupBox2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
@@ -164,57 +120,11 @@ Partial Class frmBackupeRestauracao
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Restauracao"
         '
-        'btnRestaurar
-        '
-        Me.btnRestaurar.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnRestaurar.Location = New System.Drawing.Point(260, 194)
-        Me.btnRestaurar.Name = "btnRestaurar"
-        Me.btnRestaurar.Size = New System.Drawing.Size(245, 66)
-        Me.btnRestaurar.TabIndex = 8
-        Me.btnRestaurar.Text = "Iniciar a Restauração"
-        Me.btnRestaurar.UseVisualStyleBackColor = True
-        '
-        'btnDestinoRestaurar
-        '
-        Me.btnDestinoRestaurar.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnDestinoRestaurar.Location = New System.Drawing.Point(623, 120)
-        Me.btnDestinoRestaurar.Name = "btnDestinoRestaurar"
-        Me.btnDestinoRestaurar.Size = New System.Drawing.Size(75, 23)
-        Me.btnDestinoRestaurar.TabIndex = 7
-        Me.btnDestinoRestaurar.Text = "Procurar"
-        Me.btnDestinoRestaurar.UseVisualStyleBackColor = True
-        '
-        'btnArquivoBackupRestaurar
-        '
-        Me.btnArquivoBackupRestaurar.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnArquivoBackupRestaurar.Location = New System.Drawing.Point(623, 54)
-        Me.btnArquivoBackupRestaurar.Name = "btnArquivoBackupRestaurar"
-        Me.btnArquivoBackupRestaurar.Size = New System.Drawing.Size(75, 23)
-        Me.btnArquivoBackupRestaurar.TabIndex = 6
-        Me.btnArquivoBackupRestaurar.Text = "Procurar"
-        Me.btnArquivoBackupRestaurar.UseVisualStyleBackColor = True
-        '
-        'txtDestinoRestaurar
-        '
-        Me.txtDestinoRestaurar.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtDestinoRestaurar.Location = New System.Drawing.Point(129, 92)
-        Me.txtDestinoRestaurar.Name = "txtDestinoRestaurar"
-        Me.txtDestinoRestaurar.Size = New System.Drawing.Size(569, 22)
-        Me.txtDestinoRestaurar.TabIndex = 4
-        '
-        'txtArquivoBackupRestaurar
-        '
-        Me.txtArquivoBackupRestaurar.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtArquivoBackupRestaurar.Location = New System.Drawing.Point(129, 26)
-        Me.txtArquivoBackupRestaurar.Name = "txtArquivoBackupRestaurar"
-        Me.txtArquivoBackupRestaurar.Size = New System.Drawing.Size(569, 22)
-        Me.txtArquivoBackupRestaurar.TabIndex = 3
-        '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(17, 102)
+        Me.Label5.Location = New System.Drawing.Point(17, 116)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(53, 17)
         Me.Label5.TabIndex = 1
@@ -230,6 +140,122 @@ Partial Class frmBackupeRestauracao
         Me.Label6.TabIndex = 0
         Me.Label6.Text = "Arquivo Backup"
         '
+        'txtDestino
+        '
+        Me.txtDestino.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.txtDestino.Location = New System.Drawing.Point(129, 92)
+        Me.txtDestino.Name = "txtDestino"
+        Me.txtDestino.ReadOnly = True
+        Me.txtDestino.Size = New System.Drawing.Size(565, 27)
+        Me.txtDestino.TabIndex = 10
+        '
+        'btnBancodeDados
+        '
+        Me.btnBancodeDados.BackColor = System.Drawing.Color.White
+        Me.btnBancodeDados.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBancodeDados.Image = CType(resources.GetObject("btnBancodeDados.Image"), System.Drawing.Image)
+        Me.btnBancodeDados.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnBancodeDados.Location = New System.Drawing.Point(594, 54)
+        Me.btnBancodeDados.Name = "btnBancodeDados"
+        Me.btnBancodeDados.Size = New System.Drawing.Size(100, 32)
+        Me.btnBancodeDados.TabIndex = 11
+        Me.btnBancodeDados.Text = "Procurar"
+        Me.btnBancodeDados.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnBancodeDados.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(594, 125)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 32)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Procurar"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'btnBackup
+        '
+        Me.btnBackup.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnBackup.Image = CType(resources.GetObject("btnBackup.Image"), System.Drawing.Image)
+        Me.btnBackup.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnBackup.Location = New System.Drawing.Point(318, 226)
+        Me.btnBackup.Name = "btnBackup"
+        Me.btnBackup.Size = New System.Drawing.Size(159, 48)
+        Me.btnBackup.TabIndex = 13
+        Me.btnBackup.Text = "Iniciar Backup"
+        Me.btnBackup.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnBackup.UseVisualStyleBackColor = False
+        '
+        'btnRestaurar
+        '
+        Me.btnRestaurar.BackColor = System.Drawing.Color.White
+        Me.btnRestaurar.Image = CType(resources.GetObject("btnRestaurar.Image"), System.Drawing.Image)
+        Me.btnRestaurar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnRestaurar.Location = New System.Drawing.Point(293, 203)
+        Me.btnRestaurar.Name = "btnRestaurar"
+        Me.btnRestaurar.Size = New System.Drawing.Size(205, 56)
+        Me.btnRestaurar.TabIndex = 8
+        Me.btnRestaurar.Text = "Iniciar Restauração"
+        Me.btnRestaurar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnRestaurar.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.Location = New System.Drawing.Point(594, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 32)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Procurar"
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
+        Me.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button3.Location = New System.Drawing.Point(594, 149)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 32)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Procurar"
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'txtArquivoBackupRestaurar
+        '
+        Me.txtArquivoBackupRestaurar.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.txtArquivoBackupRestaurar.Location = New System.Drawing.Point(125, 26)
+        Me.txtArquivoBackupRestaurar.Name = "txtArquivoBackupRestaurar"
+        Me.txtArquivoBackupRestaurar.ReadOnly = True
+        Me.txtArquivoBackupRestaurar.Size = New System.Drawing.Size(569, 27)
+        Me.txtArquivoBackupRestaurar.TabIndex = 15
+        '
+        'txtDestinoRestaurar
+        '
+        Me.txtDestinoRestaurar.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.txtDestinoRestaurar.Location = New System.Drawing.Point(125, 116)
+        Me.txtDestinoRestaurar.Name = "txtDestinoRestaurar"
+        Me.txtDestinoRestaurar.ReadOnly = True
+        Me.txtDestinoRestaurar.Size = New System.Drawing.Size(569, 27)
+        Me.txtDestinoRestaurar.TabIndex = 16
+        '
+        'txtNomeBackup
+        '
+        Me.txtNomeBackup.Location = New System.Drawing.Point(133, 173)
+        Me.txtNomeBackup.Name = "txtNomeBackup"
+        Me.txtNomeBackup.Size = New System.Drawing.Size(384, 27)
+        Me.txtNomeBackup.TabIndex = 14
+        '
         'frmBackupeRestauracao
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -237,6 +263,7 @@ Partial Class frmBackupeRestauracao
         Me.ClientSize = New System.Drawing.Size(767, 625)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmBackupeRestauracao"
         Me.Text = "Backup e Restauraçao"
         Me.GroupBox1.ResumeLayout(False)
@@ -247,21 +274,21 @@ Partial Class frmBackupeRestauracao
 
     End Sub
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents btnBackup As System.Windows.Forms.Button
-    Friend WithEvents btnDestino As System.Windows.Forms.Button
-    Friend WithEvents btnBancoDeDados As System.Windows.Forms.Button
-    Friend WithEvents txtNomeBackup As System.Windows.Forms.TextBox
-    Friend WithEvents txtDestino As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
-    Friend WithEvents btnRestaurar As System.Windows.Forms.Button
-    Friend WithEvents btnDestinoRestaurar As System.Windows.Forms.Button
-    Friend WithEvents btnArquivoBackupRestaurar As System.Windows.Forms.Button
-    Friend WithEvents txtDestinoRestaurar As System.Windows.Forms.TextBox
-    Friend WithEvents txtArquivoBackupRestaurar As System.Windows.Forms.TextBox
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtBancoDeDados As System.Windows.Forms.TextBox
+    Friend WithEvents btnBancodeDados As System.Windows.Forms.Button
+    Friend WithEvents txtDestino As System.Windows.Forms.TextBox
+    Friend WithEvents btnBackup As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtArquivoBackupRestaurar As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnRestaurar As System.Windows.Forms.Button
+    Friend WithEvents txtNomeBackup As System.Windows.Forms.TextBox
+    Friend WithEvents txtDestinoRestaurar As System.Windows.Forms.TextBox
 End Class
